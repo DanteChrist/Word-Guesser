5324a2f73504baf60dc23396884b54e9e841d86c
DanteChrist <dantewrites000@gmail.com>
2024-04-01 15:02:25 +0300

    add TranslationDTO and BaseDTO, create Profile folder, create ITrabslationsService interface

---
+++ Word-Guesser.Services/Abstarctions/ITranslationsService.cs
@@ -0,0 +1,14 @@
+﻿using Word_Guesser.Services.DTOs;
+
+namespace Word_Guesser.Services.Abstarctions
+{
+    public interface ITranslationsService
+    {
+        Task<List<TranslationDTO>> GetTranslationsAsync();
+        Task<TranslationDTO> GetTranslationsByIdAsync(int id);
+        Task<List<TranslationDTO>> GetTranslationsBValueAsync(string value);
+        Task AddTranslationsAsync(TranslationDTO translation);
+        Task DeleteTranslationsByIdAsync(int id);
+        Task UpdateTranslationsAsync(TranslationDTO translation);
+    }
+}
