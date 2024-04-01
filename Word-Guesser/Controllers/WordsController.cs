7b7bc7716439c85869edc38191ce22def8356ce1
DanteChrist <dantewrites000@gmail.com>
2024-04-01 13:50:53 +0300

    add Words controller

---
+++ Word-Guesser/Controllers/WordsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Word_Guesser.Data;
+using Word_Guesser.Data.Data.Entities;
+
+namespace Word_Guesser.Controllers
+{
+    public class WordsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WordsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Words
+        public async Task<IActionResult> Index()
+        {
+              return _context.Words != null ? 
+                          View(await _context.Words.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Words'  is null.");
+        }
+
+        // GET: Words/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Words == null)
+            {
+                return NotFound();
+            }
+
+            var word = await _context.Words
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (word == null)
+            {
+                return NotFound();
+            }
+
+            return View(word);
+        }
+
+        // GET: Words/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Words/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Identifier,Id")] Word word)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(word);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(word);
+        }
+
+        // GET: Words/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Words == null)
+            {
+                return NotFound();
+            }
+
+            var word = await _context.Words.FindAsync(id);
+            if (word == null)
+            {
+                return NotFound();
+            }
+            return View(word);
+        }
+
+        // POST: Words/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Identifier,Id")] Word word)
+        {
+            if (id != word.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(word);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WordExists(word.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(word);
+        }
+
+        // GET: Words/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Words == null)
+            {
+                return NotFound();
+            }
+
+            var word = await _context.Words
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (word == null)
+            {
+                return NotFound();
+            }
+
+            return View(word);
+        }
+
+        // POST: Words/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Words == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Words'  is null.");
+            }
+            var word = await _context.Words.FindAsync(id);
+            if (word != null)
+            {
+                _context.Words.Remove(word);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WordExists(int id)
+        {
+          return (_context.Words?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
